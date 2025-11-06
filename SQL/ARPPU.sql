e8ae9e4ef3a19ba707434c9ee7cb56dcb4ba21b7
sgantzer12 <74564975+sgantzer12@users.noreply.github.com>
2025-11-06 14:24:58 +0100

    fixed ARPU and ARPPU

--- SQL/ARPPU.sql
+++ SQL/ARPPU.sql
@@ -1,40 +1,29 @@
-WITH
-  UserFirstVisit AS (
+WITH 
+TotalRevenue AS (
     SELECT
-      UserID,
-      CAST(MIN(StartTime) AS DATE) AS first_visit_date
-    FROM
-      Sessions
-    GROUP BY
-      UserID
-  ),
-  
-  UserTotalRevenue AS (
-    SELECT
-      s.UserID,
-      SUM(r.RevenueAmount) AS total_user_revenue
-    FROM
-      Revenues AS r
-      JOIN Sessions AS s ON r.SessionID = s.SessionID
-    GROUP BY
-      s.UserID
-  )
+        SUM(I.Price) AS TotalSumRevenue
+    FROM Revenues AS R
+    JOIN Items AS I 
+        ON R.ItemID = I.ItemID
+),
 
+PayingUsers AS (
+    SELECT
+        DISTINCT S.UserID
+    FROM Revenues AS R
+    JOIN Sessions AS S 
+        ON R.SessionID = S.SessionID
+),
+
+PayingUserCount AS (
+    SELECT
+        COUNT(UserID) AS TotalPayingUsers
+    FROM PayingUsers
+)
 SELECT
-  f.first_visit_date AS cohort_date,
-  
-  SUM(COALESCE(r.total_user_revenue, 0)) AS cohort_total_revenue,
-  
-  COUNT(DISTINCT r.UserID) AS cohort_paying_users,
-
-  (
-    SUM(COALESCE(r.total_user_revenue, 0)) / 
-    NULLIF(COUNT(DISTINCT r.UserID), 0)
-  ) AS cohort_arppu
-FROM
-  UserFirstVisit AS f
-  LEFT JOIN UserTotalRevenue AS r ON f.UserID = r.UserID
-GROUP BY
-  f.first_visit_date
-ORDER BY
-  cohort_date DESC;
+    TR.TotalSumRevenue,
+    PC.TotalPayingUsers,
+    TR.TotalSumRevenue / NULLIF(PC.TotalPayingUsers, 0) AS ARPPU
+FROM 
+    TotalRevenue AS TR,
+    PayingUserCount AS PC;
--- SQL/ARPU.sql
+++ SQL/ARPU.sql
@@ -1,36 +1,14 @@
-WITH
-  UserFirstVisit AS (
+WITH TotalRevenue AS (
     SELECT
-      UserID,
-      CAST(MIN(StartTime) AS DATE) AS first_visit_date
-    FROM
-      Sessions
-    GROUP BY
-      UserID
-  ),
-  
-  UserTotalRevenue AS (
+        SUM(I.Price) AS TotalSumRevenue
+    FROM Revenues AS R
+    JOIN Items AS I ON R.ItemID = I.ItemID
+),
+TotalUsers AS (
     SELECT
-      s.UserID,
-      SUM(r.RevenueAmount) AS total_user_revenue
-    FROM
-      Revenues AS r
-      JOIN Sessions AS s ON r.SessionID = s.SessionID
-    GROUP BY
-      s.UserID
-  )
-
+        COUNT(DISTINCT UserID) AS TotalUserCount
+    FROM users
+)
 SELECT
-  f.first_visit_date AS cohort_date,
-  COUNT(DISTINCT f.UserID) AS cohort_size,
-  SUM(COALESCE(r.total_user_revenue, 0)) AS cohort_total_revenue,
-  (
-    SUM(COALESCE(r.total_user_revenue, 0)) / COUNT(DISTINCT f.UserID)
-  ) AS cohort_arpu
-FROM
-  UserFirstVisit AS f
-  LEFT JOIN UserTotalRevenue AS r ON f.UserID = r.UserID
-GROUP BY
-  f.first_visit_date
-ORDER BY
-  cohort_date DESC;
+    TR.TotalSumRevenue / NULLIF(TU.TotalUserCount, 0) AS ARPU
+FROM TotalRevenue AS TR, TotalUsers AS TU;
